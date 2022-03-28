5609ff96255ae81936c4832f96e86d5f8e04fff4
Siam Islam <30402883+siamislam1603@users.noreply.github.com>
2022-03-28 14:57:07 +0600

    Added posts router,controller & model

--- server/index.js
+++ server/index.js
@@ -2,10 +2,17 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import { CONNECTION_URL } from './connection_url';
+import { CONNECTION_URL } from './connection_url.js';
+import router from './routes/posts.js';
 
 const app=express();
 
 app.use(bodyParser.json({limit:'30mb',extended:true}));
 app.use(bodyParser.urlencoded({limit:'30mb',extended:true}));
 app.use(cors());
+app.use('/posts',router);
+
+const PORT=process.env.PORT || 5000;
+mongoose.connect(CONNECTION_URL,{useNewUrlParser:true,useUnifiedTopology:true})
+    .then(()=>app.listen(PORT,()=>console.log(`Server running on port: ${PORT}`)))
+    .catch((err)=>console.log(err.message));
